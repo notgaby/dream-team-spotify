7142bd06fa945744899cc732088cd28cc1430bf3
Collin Newman <45407009+jetskipenguin@users.noreply.github.com>
2022-04-14 18:47:20 -0500

    add spotify auth button

--- spotifyteam/src/App.js
+++ spotifyteam/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { SpotifyAuth, Scopes } from 'react-spotify-auth'
+import 'react-spotify-auth/dist/index.css' // if using the included styles
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SpotifyAuth
+    redirectUri='http://localhost:3000/callback'
+    clientID='bd61cf3a1de2417cb6e82bb5b740b489'
+    scopes={[Scopes.userReadPrivate, Scopes.userReadEmail]}
+    />
   );
 }
 
